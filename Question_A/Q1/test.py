9ca636d24e9f33005c62d20533331bddfee84adb
WangYue <yw.robotics@gmail.com>
2022-10-06 11:37:32 +0800

    new commit

--- Question_A/Q1/test.py
+++ Question_A/Q1/test.py
@@ -1,31 +1,11 @@
 import numpy as np
+import os
 
-loadData = np.load('data_q1.npy')
+pwd = os.getcwd()
+print(pwd)
+loadData = np.load('../data_q1.npy')
 
 print("----type----")
 print(type(loadData))
 print("----shape----")
 print(loadData.shape)
-# print("----data----")
-# print(loadData)
-
-loadData1 = np.load('data_q2.npy')
-
-print("----type----")
-print(type(loadData1))
-print("----shape----")
-print(loadData1.shape)
-
-loadData2 = np.load('data_q3.npy')
-
-print("----type----")
-print(type(loadData2))
-print("----shape----")
-print(loadData2.shape)
-
-loadData3 = np.load('data_q4.npy')
-
-print("----type----")
-print(type(loadData3))
-print("----shape----")
-print(loadData3.shape)
